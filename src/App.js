06e374194a8856bcf3d8f487a83f2f3d5598173f
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-18 21:15:15 -0500

    Import header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Route, Routes } from 'react-router';
 import Home from './Home';
+import Header from './Header'
 
 function App() {
   return (
     <main className="App">
+      {/* <Header/> */}
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
