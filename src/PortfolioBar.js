a38dbf28f18ebccbb6b752ada36056a62c9b4856
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-18 19:38:35 -0500

    Add component

---
+++ src/PortfolioBar.js
@@ -0,0 +1,11 @@
+import './PortfolioBar.css'
+
+function PortfolioBar () {
+  return (
+    <section className='PortfolioBar'>
+      <p>hello</p>
+    </section>
+  )
+}
+
+export default PortfolioBar
