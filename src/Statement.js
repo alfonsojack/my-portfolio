665a4daf6ade8f357193053908003d8493cf492c
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-18 19:39:49 -0500

    Add component

---
+++ src/Statement.js
@@ -0,0 +1,14 @@
+import './Statement.css'
+
+function Statement () {
+  return (
+    <div className='img-overlay'>
+    <div className='Statement'>
+      <h1>Hi. My name is Jack Alfonso</h1>
+      <h2>Frontend Developer</h2>
+    </div>
+    </div>
+  )
+}
+
+export default Statement
