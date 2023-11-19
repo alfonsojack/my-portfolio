8c8bc523b94d67e28865d13878b3e28be402a1dc
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-18 23:04:41 -0500

    Add styling

--- src/Statement.js
+++ src/Statement.js
@@ -6,6 +6,15 @@ function Statement () {
     <div className='Statement'>
       <h1>Hi. My name is Jack Alfonso</h1>
       <h2>Frontend Developer</h2>
+      <div className='stack'>  
+<a href="https://reactjs.org/" target="_blank"><img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="React" height="50" /></a>  
+<a href="https://www.w3schools.com/css/" target="_blank"><img src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="CSS3" height="50" /></a>  
+<a href="https://en.wikipedia.org/wiki/HTML5" target="_blank"><img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5" height="50" /></a>  
+<a href="https://www.javascript.com/" target="_blank"><img src="https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white" alt="JavaScript" height="50" /></a>  
+<a href="https://www.figma.com/" target="_blank"><img  src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white" alt="Figma" height="50" /></a>  
+<a href="https://expressjs.com/" target="_blank"><img  src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB" alt="Express.js" height="50" /></a>  
+<a href="https://www.postgresql.org/" target="_blank"><img  src="https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white" alt="PostgreSQL" height="50" /></a>  
+</div>  
     </div>
     </div>
   )
