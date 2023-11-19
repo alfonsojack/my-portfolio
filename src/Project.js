585f8902113a63bd0a7701ad27bb76aef1b428c2
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-18 19:56:21 -0500

    Add images

--- src/Project.js
+++ src/Project.js
@@ -1,6 +1,8 @@
 import './Project.css'
 
 function Project() {
+  const slides = ['/Users/jack/home/pd/portfolio/src/Screenshot 2023-11-18 at 7.47.10 PM.png', '/Users/jack/home/pd/portfolio/src/Screenshot 2023-11-18 at 7.47.39 PM.png', '/Users/jack/home/pd/portfolio/src/Screenshot 2023-11-18 at 7.49.17 PM.png']
+  
   return (
     <div>
 
