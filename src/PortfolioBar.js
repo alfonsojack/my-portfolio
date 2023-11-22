73b1eb6ed5f87f1da69341e4b3e0e951d08f6229
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-22 15:42:50 -0500

    Add more info

--- src/PortfolioBar.js
+++ src/PortfolioBar.js
@@ -16,12 +16,13 @@ import rancid4 from './rancid4.png'
 function PortfolioBar () {
   const matthewsSlides = [matthews1, matthews2, matthews3, matthews4];
   const coffeeSlides = [coffeeScreenshot1, coffeeScreenshot2, coffeeScreenshot3, coffeeScreenshot4]
-  const rancidSlides = [rancid1, rancid2, rancid3, rancid4]
+  const rancidSlides = [rancid1, rancid2, rancid3, rancid4];
+  const matthewsSummary = "Matthews allows users to create new poetry by combining existing poems. They can search through a poetry database and combine a poem of their choosing with another poem of equal length or they can click a button that mixes together a random assortment of poems in the database."
 
 
   return (
     <section className='PortfolioBar'>
-      <Project slides={matthewsSlides} title="Matthews Poetry Generator"/>
+      <Project slides={matthewsSlides} title="Matthews Poetry Generator" summary={matthewsSummary} githubRepo={'https://github.com/alfonsojack/matthews'} deployLink={'https://matthews-two.vercel.app/'}/>
       <Project slides={coffeeSlides} title="Full Stack Coffee Shop Ratings"/>
       <Project slides={rancidSlides} title="Rancid Tomatillos Movie Directory"/>
     </section>
