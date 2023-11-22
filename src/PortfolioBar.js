34430b99ff0d42cfdaf44bdec9a5e14a1a1a0fdf
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-22 18:43:18 -0500

    Modify bar

--- src/PortfolioBar.js
+++ src/PortfolioBar.js
@@ -18,13 +18,16 @@ function PortfolioBar () {
   const coffeeSlides = [coffeeScreenshot1, coffeeScreenshot2, coffeeScreenshot3, coffeeScreenshot4]
   const rancidSlides = [rancid1, rancid2, rancid3, rancid4];
   const matthewsSummary = "Matthews allows users to create new poetry by combining existing poems. They can search through a poetry database and combine a poem of their choosing with another poem of equal length or they can click a button that mixes together a random assortment of poems in the database."
-
+  const coffeeSummary = "For this project, my team and I built a database storing ratings and relevant information for coffee in the Denver area. The frontend application we built allows users to click through coffee shops, view their information, and rate them accordingly."
+  const matthewsStack = ['React', 'React Router', 'HTML', 'CSS', 'Github Issues', 'Cypress E2E Testing', 'Vercel']
+  const coffeeStack = ['Knex', 'Express', 'PostgreSQL', 'React', 'React Router', 'HTML', 'CSS', 'Github Issues', 'Cypress E2E Testing', 'Vercel']
+  const rancidStack = ['React', 'React Router', 'HTML', 'CSS', 'Github Issues', 'Cypress E2E Testing', 'Vercel']
 
   return (
     <section className='PortfolioBar'>
-      <Project slides={matthewsSlides} title="Matthews Poetry Generator" summary={matthewsSummary} githubRepo={'https://github.com/alfonsojack/matthews'} deployLink={'https://matthews-two.vercel.app/'}/>
-      <Project slides={coffeeSlides} title="Full Stack Coffee Shop Ratings"/>
-      <Project slides={rancidSlides} title="Rancid Tomatillos Movie Directory"/>
+      <Project slides={matthewsSlides} title="Matthews Poetry Generator" summary={matthewsSummary} githubRepo={'https://github.com/alfonsojack/matthews'} deployLink={'https://matthews-two.vercel.app/'} stack={matthewsStack}/>
+      <Project slides={coffeeSlides} title="Full Stack Coffee Shop Ratings" summary={coffeeSummary} githubRepo={'https://github.com/alfonsojack/caphill-coffee'} deployLink={'https://caphill-coffee-brown.vercel.app/'} stack={coffeeStack}/>
+      <Project slides={rancidSlides} title="Rancid Tomatillos Movie Directory" stack={rancidStack}/>
     </section>
   )
 }
