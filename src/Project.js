05662ef819f37ecdc4ec3bf7bbb33fcee205a7ea
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-28 17:43:56 -0700

    Add About Me buttons

--- src/Project.js
+++ src/Project.js
@@ -83,20 +83,20 @@ function Project({slides, title, summary, githubRepo, deployLink, stack}) {
     <div className='slideshow moreInfo'>
       <div className='project-header'>
       <h2>{title}</h2>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon-home" onClick={handleMoreInfoClick}><path className="primary" d="M9 22H5a1 1 0 0 1-1-1V11l8-8 8 8v10a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1zm3-9a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/><path className="secondary" d="M12.01 4.42l-8.3 8.3a1 1 0 1 1-1.42-1.41l9.02-9.02a1 1 0 0 1 1.41 0l8.99 9.02a1 1 0 0 1-1.42 1.41l-8.28-8.3z"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon-home" onClick={handleMoreInfoClick}><path className="homeprimary" d="M9 22H5a1 1 0 0 1-1-1V11l8-8 8 8v10a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1v-4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v4a1 1 0 0 1-1 1zm3-9a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/><path className="homesecondary" d="M12.01 4.42l-8.3 8.3a1 1 0 1 1-1.42-1.41l9.02-9.02a1 1 0 0 1 1.41 0l8.99 9.02a1 1 0 0 1-1.42 1.41l-8.28-8.3z"/></svg>
       </div>
       <div className='project-summary'>
-      <h2>Summary</h2>
+      <h2 className='head'>Summary</h2>
       <p className='project-summary'>{summary}</p>
       </div>
       <div className='project-summary'>
-      <h2>Tech Stack</h2>
+      <h2 className='head'>Tech Stack</h2>
       <p className='project-summary'>{stack.join(', ')}</p>
       </div>
       <div className='project-buttons'>
-        <Link to={githubRepo} target="_blank" rel="noopener noreferrer">GitHub Repo</Link>
-        <Link to={deployLink} target="_blank" rel="noopener noreferrer">Deployed Site</Link>
-        <Link>Learn More</Link>
+        <Link className='a' to={githubRepo} target="_blank" rel="noopener noreferrer">GitHub Repo</Link>
+        <Link className='a' to={deployLink} target="_blank" rel="noopener noreferrer">Deployed Site</Link>
+        <Link className='a' >Learn More</Link>
       </div>
     </div>
     }
