aa06075f6edde70672b69f2ac4f2867865d51c6f
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-11-30 15:15:42 -0700

    Fix sizing

--- src/Project.js
+++ src/Project.js
@@ -94,9 +94,9 @@ function Project({slides, title, summary, githubRepo, deployLink, stack}) {
       <p className='project-summary'>{stack.join(', ')}</p>
       </div>
       <div className='project-buttons'>
-        <Link className='a' to={githubRepo} target="_blank" rel="noopener noreferrer">GitHub Repo</Link>
-        <Link className='a' to={deployLink} target="_blank" rel="noopener noreferrer">Deployed Site</Link>
-        <Link className='a' >Learn More</Link>
+        <Link className='a' to={githubRepo} target="_blank" rel="noopener noreferrer"><p className='p'>GitHub Repo</p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon-external-window"><path className="mailprimary" d="M12 8a1 1 0 0 1-1 1H5v10h10v-6a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9c0-1.1.9-2 2-2h6a1 1 0 0 1 1 1z"/><path className="mailprimary" d="M19 6.41L8.7 16.71a1 1 0 1 1-1.4-1.42L17.58 5H14a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V6.41z"/></svg></Link>
+        <Link className='a' to={deployLink} target="_blank" rel="noopener noreferrer"><p className='p'>Deployed Site</p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon-external-window"><path className="mailprimary" d="M12 8a1 1 0 0 1-1 1H5v10h10v-6a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9c0-1.1.9-2 2-2h6a1 1 0 0 1 1 1z"/><path className="mailprimary" d="M19 6.41L8.7 16.71a1 1 0 1 1-1.4-1.42L17.58 5H14a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V6.41z"/></svg></Link>
+        {/* <Link className='a' >Learn More</Link> */}
       </div>
     </div>
     }
