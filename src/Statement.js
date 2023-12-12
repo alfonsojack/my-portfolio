8620f825915d0fd4daa639315df1f97f0a899779
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-12-12 16:02:19 -0700

    Remove duplicate card

--- src/Statement.js
+++ src/Statement.js
@@ -43,7 +43,6 @@ function Statement () {
         <a href="https://reactjs.org/" target="_blank"><img className='img' src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="React"  /></a>  
         <a href="https://www.w3schools.com/css/" target="_blank"><img className='img' src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="CSS3" /></a>  
         <a href="https://en.wikipedia.org/wiki/HTML5" target="_blank"><img className='img' src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white" alt="HTML5"  /></a>  
-        <a href="https://www.javascript.com/" target="_blank"><img className='img'src="https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white" alt="JavaScript"  /></a>  
         <a href="https://www.figma.com/" target="_blank"><img className='img' src="https://img.shields.io/badge/React_Router-CA4245?style=for-the-badge&logo=react-router&logoColor=white" alt="Figma" /></a>  
         <a href="https://expressjs.com/" target="_blank"><img className='img' src="https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB" alt="Express.js" /></a>  
         <a href="https://www.javascript.com/" target="_blank"><img className='img'src="https://img.shields.io/badge/redux-%23593d88.svg?style=for-the-badge&logo=redux&logoColor=white" alt="JavaScript"  /></a>  
